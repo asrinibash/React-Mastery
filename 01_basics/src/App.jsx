16dd4fb76526a0ffb2146b9c8d51a0ecb3f6ccb0
A Srinibash Achary <126653603+asrinibash@users.noreply.github.com>
2024-01-25 20:50:14 +0530

    Disscuss how react internally works

--- 01_basics/src/App.jsx
+++ 01_basics/src/App.jsx
@@ -1,11 +1,10 @@
-
-
+import Chai from "./Chai"
 
 function App() {
 
 
   return (
-   <h1>Hello I'm Srinibash</h1>
+   <Chai/>
   )
 }
 
---
+++ 01_basics/src/Chai.jsx
@@ -0,0 +1,10 @@
+function Chai(){
+    return(
+        <>
+        <h1>Hello I'm Srinibash</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui dignissimos facilis, dolorem aliquam veritatis doloribus facere magni saepe cupiditate illo?</p>
+        </>
+    )
+}
+
+export default Chai;
